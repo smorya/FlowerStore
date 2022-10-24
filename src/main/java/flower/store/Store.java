6310ee640e6015cbc707f6aa7fbf8b1304999c03
smorya <maryana.skoropad@gmail.com>
2022-10-24 04:18:58 +0300

    store added

--- src/main/java/flower/store/Store.java
+++ src/main/java/flower/store/Store.java
@@ -1,5 +1,20 @@
 package flower.store;
 
 public class Store {
+    public void search(Flower flowerToFind) {
+        FlowerType type = flowerToFind.getFlowerType();
 
+        switch (type) {
+            case ROSE:
+                System.out.println("Here you are, rose!");
+                break;
+            case CHAMOMILE:
+                System.out.println("Here you are, chamomile!");
+                break;
+            case TULIP:
+                System.out.println("Here you are, tulip!");
+                break;
+        }
+    }
 }
+
